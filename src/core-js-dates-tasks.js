fa6569152aefa6ac77aa2b72799f331385f8fef8
Andrew <andrei.sp.93@gmail.com>
2025-03-23 21:02:46 +0300

    feat: solve task: 4-7

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -89,8 +89,15 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const getDayIndex = date.getDay();
+  const daysUntilFriday = (5 - getDayIndex + 7) % 7;
+  if (daysUntilFriday === 0) {
+    date.setDate(date.getDate() + 7);
+  } else {
+    date.setDate(date.getDate() + daysUntilFriday);
+  }
+  return date;
 }
 
 /**
@@ -104,8 +111,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month).getUTCDate();
 }
 
 /**
@@ -119,10 +126,13 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const diffInMilliseconds = end - start;
+  const countDays = diffInMilliseconds / (1000 * 3600 * 24);
+  return Math.abs(Math.round(countDays)) + 1;
 }
-
 /**
  * Returns true if a given date is within a specified range, including both the start and end dates.
  *
@@ -140,8 +150,14 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start).getTime();
+  const checkDate = new Date(date).getTime();
+  const end = new Date(period.end).getTime();
+  if (checkDate >= start && checkDate <= end) {
+    return true;
+  }
+  return false;
 }
 
 /**
